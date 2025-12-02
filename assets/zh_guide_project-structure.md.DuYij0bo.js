aa3c959dd3aa31dbb9b5fbd86a4fb68ff33e819f
LoneSurvivor <584605646@qq.com>
2025-12-02 14:44:21 +0800

    首次提交

---
+++ assets/zh_guide_project-structure.md.DuYij0bo.js
@@ -0,0 +1,9 @@
+import{_ as a,c as n,o as e,ae as p}from"./chunks/framework.CHDT0Kbg.js";const _=JSON.parse('{"title":"项目结构","description":"","frontmatter":{},"headers":[],"relativePath":"zh/guide/project-structure.md","filePath":"zh/guide/project-structure.md"}'),t={name:"zh/guide/project-structure.md"};function i(l,s,c,r,o,u){return e(),n("div",null,[...s[0]||(s[0]=[p(`<h1 id="项目结构" tabindex="-1">项目结构 <a class="header-anchor" href="#项目结构" aria-label="Permalink to &quot;项目结构&quot;">​</a></h1><p>简单分为三类目录，分别是：</p><ul><li>business：业务视图目录</li><li>screen：大屏目录</li><li>system：系统页面目录</li></ul><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>.</span></span>
+<span class="line"><span>├─ views</span></span>
+<span class="line"><span>│  ├─ business</span></span>
+<span class="line"><span>│  │  ├─ changliu</span></span>
+<span class="line"><span>│  │  ├─ ...</span></span>
+<span class="line"><span>│  │  └─ keystone</span></span>
+<span class="line"><span>│  ├─ screen</span></span>
+<span class="line"><span>│  └─ system</span></span>
+<span class="line"><span>└─ package.json</span></span></code></pre></div>`,4)])])}const h=a(t,[["render",i]]);export{_ as __pageData,h as default};
